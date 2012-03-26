02297fa124d82eeb4b32ff8b48c2db8dcea2326b
Alex Shkor <alex.shkor@hotmail.com>
2012-03-26 23:00:59 +0300

    mongo service setup, web config settings

---
+++ YouMap/Run-MongoRead-Server-ProBook-Install.bat
@@ -0,0 +1,2 @@
+"c:\mongo\bin\mongod.exe" -install --dbpath "c:\mongo\db" --port 27020 --nojournal --logpath "c:\mongo\logs\log.txt"
+PAUSE
--- YouMap/Run-MongoRead-Server.bat
+++ YouMap/Run-MongoRead-Server.bat
@@ -1,3 +1,3 @@
-"D:\mongo\mongodb-win32-i386-2.0.2\bin\mongod.exe" --dbpath "d:\mongo\db" --port 27020 --repair
-"D:\mongo\mongodb-win32-i386-2.0.2\bin\mongod.exe" --dbpath "d:\mongo\db" --port 27020
+"D:\mongo\mongodb-win32-i386-2.0.2\bin\mongod.exe" --dbpath "d:\mongo\db" --port 27020 --nojournal --repair
+"D:\mongo\mongodb-win32-i386-2.0.2\bin\mongod.exe" --dbpath "d:\mongo\db" --port 27020 --nojournal
 PAUSE
