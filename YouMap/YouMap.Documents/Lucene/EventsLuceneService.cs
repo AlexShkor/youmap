49fc08bf1d5b9b7c8e2beb1595071575aae4a297
Alex Shkor <alex.shkor@hotmail.com>
2012-04-06 22:42:52 +0300

    events lucene search added

---
+++ YouMap/YouMap.Documents/Lucene/EventsLuceneService.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Lucene.Net.Documents;
+using Lucene.Net.Search;
+using YouMap.Framework;
+
+namespace YouMap.Documents.Lucene
+{
+    public class EventsLuceneService: LuceneIndexService<EventLucene>
+    {
+        public EventsLuceneService(Settings settings) : base(settings)
+        {
+        }
+
+        public virtual IEnumerable<EventLucene> Search(EventLuceneFilter filter)
+        {
+            var queries = new List<Query>();
+            if (!String.IsNullOrEmpty(filter.SearchText))
+            {
+                var words = filter.SearchText.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+                queries.Add(JoinQueriesOr(
+                    BuildFuzzyQueryOr("Memo", words),
+                    BuildFuzzyQueryOr("Title", words),
+                    BuildPrefixQueryAnd("Title", words), 
+                    BuildFuzzyQueryOr("PlaceTitle", words),
+                    BuildPrefixQueryAnd("PlaceTitle", words),
+                    BuildPhraseQueryOr("MembersNames", words)));
+            }
+            if (filter.MembersIds != null && filter.MembersIds.Any())
+            {
+                queries.Add(BuildPhraseQueryOr("MembersIds", filter.MembersIds.ToArray()));
+            }
+            var query = queries.Count == 0 ? new MatchAllDocsQuery() : JoinQueriesAnd(queries.ToArray());
+            return Search(query);
+        }
+
+        protected override Document MapToLucene(EventLucene item)
+        {
+         
+            var doc = new Document();
+            doc.Add(new Field("_id", item.Id, Field.Store.YES, Field.Index.NOT_ANALYZED));
+            if (item.Title != null)
+                doc.Add(new Field("Title", item.Title, Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.WITH_POSITIONS_OFFSETS)); 
+            if (item.PlaceTitle != null)
+                doc.Add(new Field("PlaceTitle", item.PlaceTitle, Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.WITH_POSITIONS_OFFSETS));
+            if (item.Memo != null)
+                doc.Add(new Field("Memo", item.Memo, Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.WITH_POSITIONS_OFFSETS));
+            if (item.MembersIds != null && item.MembersIds.Any())
+            {
+                doc.Add(new Field("MembersIds", string.Join(" ", item.MembersIds), Field.Store.YES,
+                                  Field.Index.ANALYZED, Field.TermVector.WITH_POSITIONS_OFFSETS));
+            
+            }
+            if (item.MembersNames != null && item.MembersNames.Any())
+            {
+                doc.Add(new Field("MembersNames", string.Join(" ", item.MembersNames),
+                                  Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.WITH_POSITIONS_OFFSETS));
+            }
+            return doc;
+        }
+
+        protected override EventLucene MapFromLucene(Document doc)
+        {
+            return new EventLucene
+            {
+                Id = doc.Get("_id"),
+                Title = doc.Get("Title"),
+                Memo = doc.Get("Memo"),
+                PlaceTitle = doc.Get("PlaceTitle"),
+            };
+        }
+    }
+
+    public class EventLuceneFilter
+    {
+        public string SearchText;
+
+        public List<string> MembersIds { get; set; }
+    }
+
+    public class EventLucene
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Memo { get; set; }
+
+        public List<string> MembersIds { get; set; }
+
+        public List<string> MembersNames{ get; set; }
+
+        public string PlaceTitle { get; set; }
+    }
+}
---
+++ YouMap/YouMap.EventHandlers/EventsLuceneEventHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using Paralect.ServiceBus;
+using YouMap.Documents.Lucene;
+using YouMap.Domain.Data;
+using YouMap.Domain.Events;
+
+namespace YouMap.EventHandlers
+{
+    public class EventsLuceneEventHandler:
+        IMessageHandler<User_EventAddedEvent>,
+        IMessageHandler<User_EventMemberAddedEvent>
+    {
+        private readonly EventsLuceneService _eventsLuceneService;
+
+        public EventsLuceneEventHandler(EventsLuceneService eventsLuceneService)
+        {
+            _eventsLuceneService = eventsLuceneService;
+        }
+
+        public void Handle(User_EventAddedEvent message)
+        {
+            var doc = new EventLucene()
+                          {
+                              Id = message.Id,
+                              MembersIds = message.UsersIds.ToList(),
+                          };
+            _eventsLuceneService.Insert(doc);
+        }
+
+        public void Handle(User_EventMemberAddedEvent message)
+        {
+            var doc = _eventsLuceneService.GetById("_id",message.EventId);
+            doc.MembersIds.Add(message.NewMemberId);
+            _eventsLuceneService.Update(doc);
+        }
+    }
+}
